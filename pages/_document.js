be58ca44bb91a4c5a70dcc58c1ca5b504cb53098
alonecheer <alonecheer@hotmail.com>
2020-07-21 20:07:52 +0700

    link fonts & icon

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,13 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head />
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+          key="viewport"
+        />
+        <link rel="icon" href="/static/images/psu_icon.png"></link>
+        <link rel="stylesheet" href="/static/styles/fonts.css"></link>
         <body>
           <Main />
           <NextScript />
