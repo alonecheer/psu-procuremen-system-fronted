efd6a39d0a49823630940a04c2da93f5ccaa91ec
alonecheer <alonecheer@hotmail.com>
2020-07-21 19:47:19 +0700

    install css

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-
+import "../public/static/style/style.css";
 const index = () => {
-  return <div>Index</div>;
+  return <div className="example">Index</div>;
 };
 export default index;
