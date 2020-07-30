995c28c5fad89191f4aa4785257c0da717d68f19
alonecheer <alonecheer@hotmail.com>
2020-07-31 05:50:51 +0700

    header and responsive 375px

---
+++ pages/header/Header.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "../header/header.css";
+const Header = () => {
+  return (
+    <header>
+      <title>Header</title>
+      <div className="container">
+        <div className="nav-gid">
+          {/*-------------------------- Name web --------------------------*/}
+          <div className="name-web">
+            <a href="/">
+              <div className="link">
+                <h1>PSU procuremen system</h1>
+              </div>
+            </a>
+          </div>
+          {/*-------------------------- Menu --------------------------*/}
+          <div className="fr">
+            <ul className="menu">
+              <div className="dropdown">
+                <li>
+                  <a href="#">เพิ่มคำสั่งซื้อ</a>
+                </li>
+                <div className="dropdown-content">
+                  <a href="#">พัสดุแบบปกติ 001</a>
+                  <a href="#">พัสดุแบบเร่งด่วน 002</a>
+                </div>
+              </div>
+              <li>
+                <a href="#">จัดการข้อมูลส่วนตัว</a>
+              </li>
+              <li>
+                <a href="#">ออกจากระบบ</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ pages/homepage/home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "../header/Header"
+import "../homepage/home.css";
+const home =() =>{
+    return <div>
+        <title>Homepage</title>
+    <Header />
+    </div>
+}
+export default home;
