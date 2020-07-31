27776ed2a9837e124dcad34e06050785c2849e97
alonecheer <alonecheer@hotmail.com>
2020-07-31 15:22:27 +0700

    footer no responsive

--- pages/homepage/home.js
+++ pages/homepage/home.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "../../src/components/header/Header.js"
+import Footer from "../../src/components/footer/Footer.js"
 import "../homepage/home.css";
 const home =() =>{
     return <div>
         <title>Homepage</title>
     <Header />
+
+    <Footer />
     </div>
 }
 export default home;
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./footer.css";
+const Footer = () => {
+  return (
+    <div>
+      <title>Footer</title>
+      <footer>
+        <div className="copyright">
+          <div className="container">
+            <div className="copy-grid">
+              <div className="copytext">
+                <p>
+                  Copyright © 2020 Jetnipat Thankeatphangan - All Rights
+                  Reserved
+                </p>
+              </div>
+              <div className="creadit">
+                <p>C6035512021</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+export default Footer;
