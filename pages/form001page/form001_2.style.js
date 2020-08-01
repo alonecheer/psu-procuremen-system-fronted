50732dc523faaee9950bd9f25daa943f40c3a862
alonecheer <alonecheer@hotmail.com>
2020-08-02 04:10:50 +0700

    table responsive

--- pages/form001page/form001_2.style.js
+++ pages/form001page/form001_2.style.js
@@ -32,39 +32,43 @@ Generic Styling, for Desktops/Laptops
     border: 1px solid #ccc;
     text-align: left;
   }
-  @media only screen and (max-width: 800px) {
+  /* 
+Max width before this PARTICULAR table gets nasty
+This query will take effect for any screen smaller than 760px
+and also iPads specifically.
+*/
+  @media only screen and (max-width: 760px),
+    (min-device-width: 768px) and (max-device-width: 1024px) {
     /* Force table to not be like tables anymore */
-    #no-more-tables table,
-    #no-more-tables thead,
-    #no-more-tables tbody,
-    #no-more-tables th,
-    #no-more-tables td,
-    #no-more-tables tr {
+    table,
+    thead,
+    tbody,
+    th,
+    td,
+    tr {
       display: block;
     }
 
     /* Hide table headers (but not display: none;, for accessibility) */
-    #no-more-tables thead tr {
+    thead tr {
       position: absolute;
       top: -9999px;
       left: -9999px;
     }
 
-    #no-more-tables tr {
+    tr {
       border: 1px solid #ccc;
     }
 
-    #no-more-tables td {
+    td {
       /* Behave  like a "row" */
       border: none;
       border-bottom: 1px solid #eee;
       position: relative;
       padding-left: 50%;
-      white-space: normal;
-      text-align: left;
     }
 
-    #no-more-tables td:before {
+    td:before {
       /* Now like a table header */
       position: absolute;
       /* Top/left values mimic padding */
@@ -73,15 +77,40 @@ Generic Styling, for Desktops/Laptops
       width: 45%;
       padding-right: 10px;
       white-space: nowrap;
-      text-align: left;
-      font-weight: bold;
     }
 
     /*
 	Label the data
 	*/
-    #no-more-tables td:before {
-      content: attr(data-title);
+    td:nth-of-type(1):before {
+      content: "First Name";
+    }
+    td:nth-of-type(2):before {
+      content: "Last Name";
+    }
+    td:nth-of-type(3):before {
+      content: "Job Title";
+    }
+    td:nth-of-type(4):before {
+      content: "Favorite Color";
+    }
+    td:nth-of-type(5):before {
+      content: "Wars of Trek?";
+    }
+    td:nth-of-type(6):before {
+      content: "Secret Alias";
+    }
+    td:nth-of-type(7):before {
+      content: "Date of Birth";
+    }
+    td:nth-of-type(8):before {
+      content: "Dream Vacation City";
+    }
+    td:nth-of-type(9):before {
+      content: "GPA";
+    }
+    td:nth-of-type(10):before {
+      content: "Arbitrary Data";
     }
   }
 `;
