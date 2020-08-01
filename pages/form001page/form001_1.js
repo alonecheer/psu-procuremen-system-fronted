879a92cac512a97576f68831374073a4451919b0
alonecheer <alonecheer@hotmail.com>
2020-08-01 17:37:41 +0700

    header success responsive

--- pages/form001page/form001_1.js
+++ pages/form001page/form001_1.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Header from "../../src/components/header/Header.js";
 import Footer from "../../src/components/footer/Footer.js";
-import "../form001page/form001_1.css";
 
 const form001_1 = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
 };
 export default form001_1;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,35 +4,34 @@ import styled from "styled-components";
 const Header = () => {
   return (
     <HeaderStyleWrapper>
-      <div className="container">
-        <header className="header">
-      <a href="/" className="logo">
-        PSU procuremen system
-      </a>
-      <input className="menu_btn" type="checkbox" id="menu-btn"></input>
-      <label className="menu-icon" htmlFor="menu-btn"></label>
-      <span className="nav-icon"></span>
-      <ul className="menu">
-        <li>
-          <a href="#">พัสดุแบบปกติ 001</a>
-        </li>
-        <li>
-          <a href="#">พัสดุแบบเร่งด่วน 002</a>
-        </li>
-        <li>
-          <a href="#">ประวัติการทำรายการ</a>
-        </li>
-        <li>
-          <a href="#">ข้อมูลส่วนตัว</a>
-        </li>
-        <li>
-          <a href="#">ออกจากระบบ</a>
-        </li>
-      </ul>
-      <div>sdfdsf</div>
-    </header>
-      </div>
-    
+      <header className="header">
+        <a href="/" className="logo">
+          PSU procuremen system
+        </a>
+        <input className="menu-btn" type="checkbox" id="menu-btn"></input>
+        <label className="menu-icon" htmlFor="menu-btn">
+          <span className="nav-icon"></span>
+        </label>
+        <ul className="menu">
+          <li>
+            <a href="#">พัสดุแบบปกติ 001</a>
+          </li>
+          <li>
+            <a href="#">พัสดุแบบเร่งด่วน 002</a>
+          </li>
+          <li>
+            <a href="#">ประวัติการทำรายการ</a>
+          </li>
+          <li>
+            <a href="#">ข้อมูลส่วนตัว</a>
+          </li>
+          <li>
+            <a href="#">ออกจากระบบ</a>
+          </li>
+        </ul>
+      </header>
+      
+
     </HeaderStyleWrapper>
   );
 };
